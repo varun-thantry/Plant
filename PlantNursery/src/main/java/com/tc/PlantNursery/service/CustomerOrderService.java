6115005b061f1049e6c9b1746a4971f56c5c4d7a
sanketh <sankethebbar10@gmail.com>
2023-08-28 17:36:51 +0530

    nursery staff update orders api

--- PlantNursery/src/main/java/com/tc/PlantNursery/service/CustomerOrderService.java
+++ PlantNursery/src/main/java/com/tc/PlantNursery/service/CustomerOrderService.java
@@ -1,9 +1,12 @@
 package com.tc.PlantNursery.service;
 
 import com.tc.PlantNursery.entity.CustomerOrder;
+import com.tc.PlantNursery.entity.OrderStatus;
 import com.tc.PlantNursery.repository.CustomerOrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -18,5 +21,24 @@ public class CustomerOrderService {
 
     public List<CustomerOrder> showUserOrders(Long uid){
         return customerOrderRepo.findByUser_Id(uid);
+    }
+
+    public List<CustomerOrder> showAllOrders(){
+        return customerOrderRepo.findAll();
+    }
+
+    /*------------------- Nursery staff ----------------------*/
+    public boolean updateOrderStatus(Long oid, OrderStatus orderStatus){
+        CustomerOrder customerOrder = customerOrderRepo.findById(oid).orElse(null);
+
+        if(customerOrder != null){
+            customerOrder.setOrderStatus(orderStatus);
+            customerOrderRepo.save(customerOrder);
+            return true;
+        }else {
+            return false;
+        }
+
+
     }
 }
